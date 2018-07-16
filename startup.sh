0aa6592d520a671e5aeeefdbc4d4f8cdae485d4d
Jun.Tao <jinks.tao@gmail.com>
2018-07-16 22:56:37 +0800

    fix bug

--- startup.sh
+++ startup.sh
@@ -13,15 +13,15 @@ if [ ! -f "$FLAG_FILE" ]; then
     chmod +x $XXD_HOME/xxd
     # write env variables to config
     mv -f /tmp/xxd.conf $XXD_CONFIG
-    sed -i "s|_LISTEN_IP_|$LISTEN_IP|g"  `grep _LISTEN_IP_ -l /$XXD_CONFIG`
-    sed -i "s|_CHAT_PORT_|$CHAT_PORT|g" `grep _CHAT_PORT_ -l /$XXD_CONFIG`
-    sed -i "s|_COMMON_PORT_|$COMMON_PORT|g" `grep _COMMON_PORT_ -l /$XXD_CONFIG`
-    sed -i "s|_IS_HTTPS_|$IS_HTTPS|g" `grep _IS_HTTPS_ -l /$XXD_CONFIG`
-    sed -i "s|_UPLOAD_PATH_|$UPLOAD_PATH|g" `grep _UPLOAD_PATH_ -l /$XXD_CONFIG`
-    sed -i "s|_UPLOAD_FILE_SIZE_|$UPLOAD_FILE_SIZE|g" `grep _UPLOAD_FILE_SIZE_ -l /$XXD_CONFIG`
-    sed -i "s|_RANZHI_URL_|$RANZHI_ADDRESS:$RANZHI_PORT/xuanxuan.php,$XUANXUAN_TOKEN,default|g" `grep _RANZHI_URL_ -l /$XXD_CONFIG`
-    sed -i "s|_LOG_PATH_|$LOG_PATH|g" `grep _LOG_PATH_ -l /$XXD_CONFIG`
-    sed -i "s|_CERT_PATH_|$CERT_PATH|g" `grep _CERT_PATH_ -l /$XXD_CONFIG`
+    sed -i "s|_LISTEN_IP_|$LISTEN_IP|g"  `grep _LISTEN_IP_ -l $XXD_CONFIG`
+    sed -i "s|_CHAT_PORT_|$CHAT_PORT|g" `grep _CHAT_PORT_ -l $XXD_CONFIG`
+    sed -i "s|_COMMON_PORT_|$COMMON_PORT|g" `grep _COMMON_PORT_ -l $XXD_CONFIG`
+    sed -i "s|_IS_HTTPS_|$IS_HTTPS|g" `grep _IS_HTTPS_ -l $XXD_CONFIG`
+    sed -i "s|_UPLOAD_PATH_|$UPLOAD_PATH|g" `grep _UPLOAD_PATH_ -l $XXD_CONFIG`
+    sed -i "s|_UPLOAD_FILE_SIZE_|$UPLOAD_FILE_SIZE|g" `grep _UPLOAD_FILE_SIZE_ -l $XXD_CONFIG`
+    sed -i "s|_RANZHI_URL_|$RANZHI_ADDRESS:$RANZHI_PORT/xuanxuan.php,$XUANXUAN_TOKEN,default|g" `grep _RANZHI_URL_ -l $XXD_CONFIG`
+    sed -i "s|_LOG_PATH_|$LOG_PATH|g" `grep _LOG_PATH_ -l $XXD_CONFIG`
+    sed -i "s|_CERT_PATH_|$CERT_PATH|g" `grep _CERT_PATH_ -l $XXD_CONFIG`
     echo $FLAG_INFO > $FLAG_FILE
     echo 'unzip xxd package...done!'
 fi
